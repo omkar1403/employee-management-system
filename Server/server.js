b3e0709df142fd1a1a3f019a98ed2a721fbc2bd9
OMKAR <Omkar.deshmukh2@spit.ac.in>
2023-09-07 07:14:32 +0530

    Initial changes

---
+++ Server/server.js
@@ -0,0 +1,204 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import multer from 'multer'
+import path from 'path'
+
+
+const app = express();
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public'));
+
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
+
+app.get('/getEmployee', (req, res) => {
+    const sql = "SELECT * FROM employe";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Error: "Get employee error in sql" });
+        return res.json({ Status: "Success", Result: result })
+    })
+})
+
+con.connect(function (err) {
+    if (err) {
+        console.log("Error in Connection");
+    } else {
+        console.log("Connected");
+    }
+})
+
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ Error: "You are no Authenticated" });
+    } else {
+        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+            if (err) return res.json({ Error: "Token wrong" });
+            req.role = decoded.role;
+            req.id = decoded.id;
+            next();
+        })
+    }
+}
+
+app.get('/dashboard', verifyUser, (req, res) => {
+    return res.json({ Status: "Success", role: req.role, id: req.id })
+})
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    return res.json({ Status: "Success" });
+})
+
+app.get('/adminCount', (req, res) => {
+    const sql = "Select count(id) as admin from user";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Error: "Error in runnig query" });
+        return res.json(result);
+    })
+})
+app.get('/employeeCount', (req, res) => {
+    const sql = "Select count(id) as employee from employe";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Error: "Error in runnig query" });
+        return res.json(result);
+    })
+})
+
+app.get('/salary', (req, res) => {
+    const sql = "Select sum(salary) as sumOfSalary from employe";
+    con.query(sql, (err, result) => {
+        if (err) return res.json({ Error: "Error in runnig query" });
+        return res.json(result);
+    })
+})
+
+
+app.get('/get/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM employe where id = ?";
+    con.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Error: "Get employee error in sql" });
+        return res.json({ Status: "Success", Result: result })
+    })
+})
+
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "UPDATE employe set salary = ? WHERE id = ?";
+    con.query(sql, [req.body.salary, id], (err, result) => {
+        if (err) return res.json({ Error: "update employee error in sql" });
+        return res.json({ Status: "Success" })
+    })
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "Delete FROM employe WHERE id = ?";
+    con.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Error: "delete employee error in sql" });
+        return res.json({ Status: "Success" })
+    })
+})
+
+
+
+
+
+app.post('/login', (req, res) => {
+    const sql = "SELECT * FROM user Where email = ? AND  password = ?";
+    con.query(sql, [req.body.email, req.body.password], (err, result) => {
+        if (err) return res.json({ Status: "Error", Error: "Error in running query" });
+        if (result.length > 0) {
+            const id = result[0].id;
+            const token = jwt.sign({ role: "admin" }, "jwt-secret-key", { expiresIn: '1d' });
+            res.cookie('token', token);
+            return res.json({ Status: "Success" })
+        } else {
+            return res.json({ Status: "Error", Error: "incorrect email or password" });
+        }
+    })
+})
+
+app.post('/employeelogin', (req, res) => {
+    const sql = "SELECT * FROM employe Where email = ?";
+    con.query(sql, [req.body.email], (err, result) => {
+        if(err) return res.json({Status: "Error", Error: "Error in runnig query"});
+        if(result.length > 0) {
+            bcrypt.compare(req.body.password.toString(), result[0].password, (err, response)=> {
+                if(err) return res.json({Error: "password error"});
+                if(response) {
+                    const token = jwt.sign({role: "employee", id: result[0].id}, "jwt-secret-key", {expiresIn: '1d'});
+                    res.cookie('token', token);
+                    return res.json({Status: "Success", id: result[0].id})
+                } else {
+                    return res.json({Status: "Error", Error: "Wrong Email or Password"});
+                }
+                
+            })
+             
+     
+        } else {
+            return res.json({Status: "Error", Error: "Wrong Email or Password"});
+        }
+    })
+})
+
+
+
+
+
+app.post('/create', upload.single('image'), (req, res) => {
+    const sql = "INSERT INTO employe (`name`,`email`,`password`,`address`,`salary`,`image`) VALUES(?)";
+    bcrypt.hash(req.body.password.toString(), 10, (err, hash) => {
+        if (err) return res.json({ Error: "Error in hashing Password" });
+        const values = [
+            req.body.name,
+            req.body.email,
+            hash,
+            req.body.address,
+            req.body.salary,
+            req.file.filename
+
+        ]
+
+        con.query(sql, [values], (err, result) => {
+            if (err) return res.json({ Error: "Inside signup query" });
+            return res.json({ Status: "Success" });
+        })
+
+    })
+})
+
+
+app.listen(8081, () => {
+    console.log("Running");
+})
